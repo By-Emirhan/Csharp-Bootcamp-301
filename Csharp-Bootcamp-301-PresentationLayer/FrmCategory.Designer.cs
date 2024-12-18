cb779fb753b7d6d307db15fcc2894766c7d2263c
By-Emirhan <doganlardan190@gmail.com>
2024-12-18 14:31:03 +0300

    FormEdit

--- Csharp-Bootcamp-301-PresentationLayer/FrmCategory.Designer.cs
+++ Csharp-Bootcamp-301-PresentationLayer/FrmCategory.Designer.cs
@@ -181,6 +181,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(720, 295);
             this.Controls.Add(this.rdbPasif);
             this.Controls.Add(this.rdbAktif);
